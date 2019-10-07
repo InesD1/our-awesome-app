58c1a0f840ba585bf0cb8740c5b2a024fa55adf9
InesD1 <ines.dimassi1@gmail.com>
2019-10-07 16:18:32 +0200

    new structure

--- app.js
+++ app.js
@@ -46,5 +46,6 @@ app.locals.title = 'Express - Generated with IronGenerator'
 
 const index = require('./routes/index')
 app.use('/', index)
+app.use('/auth', require('./routes/auth/login'))
 
 module.exports = app
