066501419672a9b70f3ebf4584e9262ad4869f04
InesD1 <ines.dimassi1@gmail.com>
2019-10-10 12:23:50 +0200

    product pictures added

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-
   console.log('IronGenerator JS imported successfully!');
-
 }, false);
+
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
--- routes/products.js
+++ routes/products.js
@@ -15,11 +15,17 @@ router.get("/products", (req, res, next)=> {
         brands: {$ne:null},
         _id: {$ne:null},
     })
-    .limit(5000)
+    .limit(50)
     .select({brands:1, stores:1, product_name:1,nutrient_levels:1, nutrition_grades_tags:1, countries:1, code:1, images:1, _id:1})
         .then((products)=> {
           console.log(products)
-          res.render("products", {products, user})
+          var productsCopy = [...products]
+          debugger
+          productsCopy = productsCopy.map((product)=> {
+              product.image_url = imageStringBuilder(product)
+              return product
+          })
+          res.render("products", {products: productsCopy, user})
         })
         .catch(err=> {
           console.log(err)
@@ -30,23 +36,24 @@ router.get("/products", (req, res, next)=> {
 router.get('/product/:productName', (req, res) => {
   // Product.findById(req.params.productId) not working somehow
   Product.findOne({ product_name: req.params.productName })
-  .limit(5000)  
-  .then((product) => {
-      console.log(product)
-      res.render("product-details", {product})
+    .limit(5000)  
+    .then((product) => {
+        console.log(product)
+        const image_url = imageStringBuilder(product)
+        res.render("product-details", {product, image_url})
+        })
+      .catch((err) => {
+        res.send(err)
       })
-    .catch((err) => {
-      res.send(err)
-    })
 });
 
 /*Edit product*/
 router.get("/product/edit/:productId", (req, res)=> {
-Product.findById(req.params.productId)
-.then ((product) => {
-  res.render("edit-product-details", {product})
+  Product.findById(req.params.productId)
+    .then ((product) => {
+      res.render("edit-product", {product})
+    })
   })
-})
 
 router.post("/product/edit", (req, res)=>{
   Product.findByIdAndUpdate(req.params.productId, req.body)
@@ -61,7 +68,8 @@ router.post("/product/edit", (req, res)=>{
 module.exports = router;
 
 function imageStringBuilder(product) {
-    var productCode = product.product
+    debugger
+    var productCode = product.code
     var productCodeSplit = `${productCode.slice(0,3)}/${productCode.slice(3,6)}/${productCode.slice(6,9)}/${productCode.slice(9,13)}`
     var fileName = `front_fr.${product.images.front_fr.rev}.full.jpg`
     var imageUrl = `https://static.openfoodfacts.org/images/products/${productCodeSplit}/${fileName}`
